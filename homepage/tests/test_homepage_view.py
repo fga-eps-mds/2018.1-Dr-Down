5921c325e461a77eece40f858486e6ad8e05643e
GeovanaRamos <geovannna.1998@gmail.com>
2018-03-22 18:03:26 -0300

    Creating tests for homepage
    Adding test for status code
    
    Adding test for index view
    
    Co-authored-by: Gabriela Medeiros <gabriela.medeiros2010@hotmail.com.br>

--- homepage/tests/test_homepage_view.py
+++ homepage/tests/test_homepage_view.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse, resolve
+from homepage.views import index
 
 # Create your tests here.
+
+
+class HomepageTestCase(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_homepage_view_success_status_code(self):
+        url = reverse('index')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_home_url_resolves_index_view(self):
+        view = resolve('/')
+        self.assertEquals(view.func, index)
