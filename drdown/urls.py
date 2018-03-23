ede7566720e5afc62a18279602478644603ac824
GeovanaRamos <geovannna.1998@gmail.com>
2018-03-23 10:30:55 -0300

    Changing user paths
    Adding new user path and delete user path
    
    Co-authored-by: Gabriela Medeiros <gabriela.medeiros2010@hotmail.com.br>

--- drdown/urls.py
+++ drdown/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('user.urls')),
+    path('users/', include('user.urls')),
     path('admin/', admin.site.urls),
 ]
--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import list_users, create_user, UpdateUserView
+from .views import list_users, create_user, UpdateUserView, UserDeleteView
 
 urlpatterns = [
     path('', list_users, name='list_users'),
     path('new/', create_user, name='create_user'),
-    path('edit/<int:pk>', UpdateUserView.as_view(), name='update_user')
+    path('edit/<int:pk>', UpdateUserView.as_view(), name='update_user'),
+    path('delete/<int:pk>', UserDeleteView.as_view(), name='delete_user'),
 
 ]
