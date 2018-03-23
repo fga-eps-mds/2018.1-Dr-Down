dae1683929102a06212267e7e96fb43b1d82a565
Daniel Maike <danmke@hotmail.com>
2018-03-23 11:24:01 -0300

    Correcting PEP8 errors
    Correcting PEP8 errors in test_models.py
    
    Co-authored-by: Guilherme Guy <guilhermeguy349@gmail.com>

--- user/tests/test_models.py
+++ user/tests/test_models.py
@@ -1,12 +1,14 @@
 from django.test import TestCase
 from user.forms import UserForm
+from user.models import User
 
 
 class Setup_Class(TestCase):
 
     def setUp(self):
         self.user = User.objects.create(name="Joao", last_name="Silva",
-                                        email="user@example.com", password="12345678",
+                                        email="user@example.com",
+                                        password="12345678",
                                         birthday="01/01/1997", gender="M",
                                         telephone=12345678)
 
@@ -16,7 +18,8 @@ class User_Form_Test(TestCase):
     # Valid Form Data
     def test_UserForm_valid(self):
         form = UserForm(data={'name': "Joao", 'last_name': "Silva",
-                              'email': "user@example.com", 'password': "12345678",
+                              'email': "user@example.com",
+                              'password': "12345678",
                               'birthday': "01/01/1997", 'gender': "M",
                               'telephone': 12345678})
         self.assertTrue(form.is_valid())
