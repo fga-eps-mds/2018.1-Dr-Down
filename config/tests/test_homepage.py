bc7491a7dd958cd4762606e70cec7396fef5c74e
guilherme1guy <guilhermeguy349@gmail.com>
2018-03-29 21:26:10 -0300

    Creating tests for 'About SD'
    Co-authored-by: Gabriela Medeiros <gabriela.medeiros2010@hotmail.com.br>

---
+++ config/tests/test_homepage.py
@@ -0,0 +1,29 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+
+
+class HomepageTestCase(TestCase):
+
+    def setUp(self):
+        """
+            Prepares data for tests
+        """
+
+        self.client = Client() 
+
+    def test_homepage_view_success_status_code(self): 
+        """
+            Checks if 'About SD' view is loaded successfully
+        """
+
+        url = reverse('about')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_home_url_resolves_index_view(self):
+        """
+            Checks if the '/about' url is returning the 'About SD' view
+        """
+
+        response = self.client.get(reverse('about'))
+        self.assertTemplateUsed(response, 'pages/about.html')
