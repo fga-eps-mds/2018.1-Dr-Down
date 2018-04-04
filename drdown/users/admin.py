f72b61710356057a84851c13ae12c885be853079
Daniel Maike <danmke@hotmail.com>
2018-04-04 14:40:06 -0300

    Adding Patient in admin.py
    Co-authored-by: Elias Bernardo <ebmm01@gmail.com>

--- drdown/users/admin.py
+++ drdown/users/admin.py
@@ -2,7 +2,9 @@ from django import forms
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as AuthUserAdmin
 from django.contrib.auth.forms import UserChangeForm, UserCreationForm
-from .models import User
+from .models import User, Patient
+
+admin.site.register(Patient)
 
 
 class MyUserChangeForm(UserChangeForm):
