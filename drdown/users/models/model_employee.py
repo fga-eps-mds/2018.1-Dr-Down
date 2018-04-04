2a1094f7c7bc8a8b29029aeb925a27b5883d4d07
guilherme1guy <guilhermeguy349@gmail.com>
2018-04-04 15:35:54 -0300

    Setting departament field on Employee to use choices
    This will limit the input content on this field
    
    Co-authored-by: Geovana Ramos <geovannna.1998@gmail.com>

--- drdown/users/models/model_employee.py
+++ drdown/users/models/model_employee.py
@@ -15,15 +15,44 @@ class Employee(models.Model):
         validators=[validate_cpf],
         max_length=14
     )
+    # fonoaudiologia, terapia ocupacional, cardiologia, neurologia, pediatria,  psicólogos e fisioterapia
+    
+    # this is separated from the list because of Django standars
+    # if we leave like this we can access the choices from outside
+    # example: employee.SPEECH_THERAPHY
+    # note: those texts aren't using _() because they are not meant to be translated nor
+    # shown to the user
+    SPEECH_THERAPHY = "SP_TH"
+    OCCUPATIONAL_THERAPY = "OC_TH"
+    CARDIOLOGY = "CARD"
+    NEUROLOGY = "NEURO"
+    PEDIATRICS = "PED"
+    PSYCHOLOGY = "PSY"
+    PHYSIOTHERAPY = "PHYS"
+    SECRETAY = "SEC"
+    OTHER = "OTH"
+
+    DEPARTAMENT_CHOICES = (
+        (SPEECH_THERAPHY, _('Speech Therapy')),
+        (OCCUPATIONAL_THERAPY, _('Occupational Therapy')),
+        (CARDIOLOGY, _('Cardiology')),
+        (NEUROLOGY, _('Neurology')),
+        (PEDIATRICS, _('Pediatrics')),
+        (PSYCHOLOGY, _('Psychology')),
+        (PHYSIOTHERAPY, _('Physiotherapy')),
+        (SECRETAY, _('Secretary')),
+        (OTHER, _('Other')),
+    )
 
     departament = models.CharField(
         null=False,
+        choices=DEPARTAMENT_CHOICES,
         help_text=_("The departament where this user works."),
         max_length=30
     )
 
     def __str__(self):
-        return self.user.get_username() + " - " + self.departament
+        return self.user.get_username() + " - " + self.get_departament_display()
 
     def save(self, *args, **kwargs):
 
