74b8c2bda5191594b27b0ce671ccbb6959b35d82
Daniel Maike <danmke@hotmail.com>
2018-04-04 21:39:19 -0300

    Adding test to SES number validators
    Co-authored-by: Elias Bernardo <ebmm01@gmail.com>

--- drdown/utils/tests/test_validators.py
+++ drdown/utils/tests/test_validators.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.core.exceptions import ValidationError
 
-from drdown.utils.validators import validate_cpf
+from drdown.utils.validators import validate_cpf, validate_ses
 
 
 class TestValidator(TestCase):
@@ -33,9 +33,43 @@ class TestValidator(TestCase):
         for i in range(0, wrong_test_values.__len__()):
             with self.assertRaises(ValidationError, msg=wrong_test_values[i]):
                 validate_cpf(wrong_test_values[i])
-          
+
         # test if cfp is rigth
         try:
             validate_cpf(value="974.220.200-16")
+        except ValidationError as error:
+            self.fail(msg=error.message)
+
+    def test_validate_ses(self):
+
+        wrong_test_values = [
+            "12345678",
+            "123456",
+            "1123456789",
+            "12345",
+            "1234",
+            "123",
+            "12",
+            "1",
+            "0",
+            "111111111",
+            "1111111",
+            "122345",
+            "asdqweqwe",
+            "qweqweq",
+            "qwe123qwe",
+            "qwe1234",
+            "12345678Z",
+            "123456Z",
+            ""
+        ]
+
+        for i in range(0, wrong_test_values.__len__()):
+            with self.assertRaises(ValidationError, msg=wrong_test_values[i]):
+                validate_ses(wrong_test_values[i])
+
+        # test if ses is rigth
+        try:
+            validate_ses(value="000796544")
         except ValidationError as error:
             self.fail(msg=error.message)
