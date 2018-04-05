65ef313992ab88a55f352a64a3d2cee0bb61c5b3
Gabriela Medeiros <gabriela.medeiros2010@hotmail.com.br>
2018-04-05 18:59:04 -0300

    Correcting import's name
    Co-authored-by: joberth rogers <joberth.rogers18@gmail.com>

--- drdown/users/admin.py
+++ drdown/users/admin.py
@@ -2,8 +2,10 @@ from django import forms
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as AuthUserAdmin
 from django.contrib.auth.forms import UserChangeForm, UserCreationForm
-from .models import User
+from .models import User 
+from .models.model_doctor import Doctor
 
+admin.site.register(Doctor)
 
 class MyUserChangeForm(UserChangeForm):
     class Meta(UserChangeForm.Meta):
