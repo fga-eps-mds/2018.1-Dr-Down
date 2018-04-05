63b2448e7574511247a2ded77a59a5f9ab29766a
guilherme1guy <guilhermeguy349@gmail.com>
2018-04-04 21:34:39 -0300

    Improve tests of model Employee
    Change test to amplify its coverage
    
    Co-authored-by: Geovana Ramos <geovannna.1998@gmail.com>

--- drdown/users/tests/test_model_employee.py
+++ drdown/users/tests/test_model_employee.py
@@ -1,4 +1,6 @@
 from test_plus.test import TestCase
+from django.contrib.auth.models import Group
+
 
 from ..models import Employee
 
@@ -19,10 +21,6 @@ class TestModelEmployee(TestCase):
         self.assertIs(self.user, self.employee.user)
         self.assertIs(self.employee, self.user.employee)
 
-    def test_save_making_changes_on_user(self):
-        self.employee.save()
-        self.assertEquals(self.user.is_staff, True)
-
     def test_delete_cascade(self):
 
         self.assertEquals(Employee.objects.get(cpf="974.220.200-16"), self.employee)
@@ -31,3 +29,27 @@ class TestModelEmployee(TestCase):
 
         with self.assertRaises(Employee.DoesNotExist):
             Employee.objects.get(cpf="974.220.200-16")
+
+
+class TestModelEmployeeNoSetUp(TestCase):
+
+    def test_save_making_changes_on_user(self):
+
+        # this test should have no setup executed before it
+
+        self.user = self.make_user()
+
+        self.assertEquals(self.user.is_staff, False)
+
+        with self.assertRaises(Group.DoesNotExist):
+            self.user.groups.get(name=Employee.GROUP_NAME)
+
+        # now we add the employee<--->user relation
+        self.employee = Employee.objects.create(cpf="974.220.200-16", user=self.user, departament=Employee.NEUROLOGY)
+
+        # it should create the group
+        employee_group = Group.objects.get(name=Employee.GROUP_NAME)
+
+        # and change things in the user
+        self.assertEquals(self.user.is_staff, True)
+        self.assertEqual(self.user.groups.get(name=Employee.GROUP_NAME), employee_group)
