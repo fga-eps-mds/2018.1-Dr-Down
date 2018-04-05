1bc7000877ea999098806379beaadbb37560c499
Gabriela Medeiros <gabriela.medeiros2010@hotmail.com.br>
2018-04-05 09:26:14 -0300

    Editing cpf field in doctor model
    Adding choices to speciality atribute
    
    joberth rogers <joberth.rogers18@gmail.com>

--- drdown/users/models/model_doctor.py
+++ drdown/users/models/model_doctor.py
@@ -7,17 +7,8 @@ from .model_user import User
 
 
 class Doctor(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
-    cpf = models.CharField(
-        help_text=_(
-            "Please, use enter a valid CPF in the following format: XXX.XXX.XXX-XX"),
-        unique=True,
-        validators=[validate_cpf],
-        max_length=14
-    )
-
-    SPEECH_THERAPHY = "SP_TH"
+	SPEECH_THERAPHY = "SP_TH"
     OCCUPATIONAL_THERAPY = "OC_TH"
     CARDIOLOGY = "CARD"
     NEUROLOGY = "NEURO"
@@ -35,39 +26,31 @@ class Doctor(models.Model):
         (PHYSIOTHERAPY, _('Physiotherapy')),
     )
 
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    cpf = models.CharField(
+    	choices=SPECIALITY_CHOICES,
+        help_text=_(
+            "Please, use enter a valid CPF in the following format: XXX.XXX.XXX-XX"),
+        unique=True,
+        validators=[validate_cpf],
+        max_length=14
+    )
+
     crm = models.IntegerField(
         max_length=30,
         blank=True
     )
     speciality = models.CharField(
-        choices=SPECIALITY_CHOICES,
-    	help_text=_("The speciality that this doctor works."),
+        help_text=_("The speciality that this doctor works."),
         max_length=30,
         blank=True
     )
 
-     # const representig the name of the group wich this model will add to the related user
-    GROUP_NAME = "Doctors"
 
 def __str__(self):
     return self.user.get_username() + " - " + self.get_departament_display()
 
-def save(self, *args, **kwargs):
-
-    # we wan't to add the required permissions to the related user, before saving
-    self.user.is_staff = True
-
-    try:
-        doctor_group = Group.objects.get(name=Doctor.GROUP_NAME)
-    except Group.DoesNotExist:
-        doctor_group = Group.objects.create(name=Doctor.GROUP_NAME)
-
-    # TODO: add permissions to edit Patient and Parent when they get ready
-    self.user.groups.add(doctor_group)
-
-    self.user.save()
-
-    super().save(*args, **kwargs)
 
 class Meta:
     verbose_name = _('Doctor')
