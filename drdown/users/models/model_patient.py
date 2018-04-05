02e732ebd3182e68268c3e9a2726de041d01b55e
Daniel Maike <danmke@hotmail.com>
2018-04-05 18:10:13 -0300

    Implementing model patient
    Adding new attributes to model patient
    
    Co-authored-by: Elias Bernardo <ebmm01@gmail.com>

--- drdown/users/models/model_patient.py
+++ drdown/users/models/model_patient.py
@@ -6,9 +6,9 @@ from .model_user import User
 
 
 class Patient(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)    
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     ses = models.CharField(
-        help_text=_("Please, enter the valid SES"),
+        help_text=_("Please, enter the valid SES number"),
         unique=True,
         max_length=9,
         validators=[validate_ses],
@@ -21,9 +21,46 @@ class Patient(models.Model):
         (1, _('Emerging')),
       )
     priority = models.IntegerField(
-        default=5, choices=PRIORITIES,
+        choices=PRIORITIES,
         help_text=_("Please, insert the degree of priority of the patient"),
         )
+    mother_name = models.CharField(
+                help_text=_("Please, insert your mother name"),
+                max_length=80,
+    )
+    father_name = models.CharField(
+                help_text=_("Please, insert your father name"),
+                max_length=80,
+    )
+    COLOR = (
+        (5, _('White')),
+        (4, _('Black')),
+        (3, _('Yellow')),
+        (2, _('Brown')),
+        (1, _('Indigenous')),
+    )
+    ethnicity = models.IntegerField(
+        choices=COLOR,
+        help_text=_("Please insert the ethnicity of the patient"),
+    )
+    sus_number = models.CharField(
+        help_text=_("Please, enter the valid SUS number"),
+        unique=True,
+        default='',
+        max_length=11,
+    )
+    civil_registry_of_birth = models.CharField(
+        help_text=_("Please, enter the civil registry of birth number"),
+        unique=True,
+        default='',
+        max_length=11,
+    )
+    declaration_of_live_birth = models.CharField(
+        help_text=_("Please, enter the declaration of live birth number"),
+        unique=True,
+        default='',
+        max_length=11,
+    )
 
     def __str__(self):
         return self.user.get_username()
