ed390a24216f3ec1690350cb3764260462271e8e
Guilherme Guy <guilhermeguy349@gmail.com>
2018-04-06 14:02:18 +0000

    Fixing PEP8 errors on validators.py
    Co-authored-by: Geovana Ramos <geovannna.1998@gmail.com>

--- drdown/utils/validators.py
+++ drdown/utils/validators.py
@@ -7,12 +7,15 @@ import datetime
 
 def validate_cpf(value):
     """
-        validate a CPF passed as a string, checks for correct format and valida math
+        validate a CPF passed as a string, checks for correct format and
+         valida math
     """
 
     # regex to check if CPF is XYZ.XYZ.XYZ-XX (X is a nunber from 0 to 9)
     regex_code = r"[0-9]{3}[\.][0-9]{3}[\.][0-9]{3}[-][0-9]{2}"
-    regex_validator = RegexValidator(regex=regex_code, message=_('Wrong CPF format'))
+    regex_validator = RegexValidator(
+        regex=regex_code,
+        message=_('Wrong CPF format'))
 
     # this will raise an exception in case of failure
     regex_validator(value)
@@ -20,7 +23,8 @@ def validate_cpf(value):
     # regex to check if CPF has all numbers equal
     regex_code_all_equal = r"([\d])\1\1[.]([\d])\2\2[.]([\d])\3\3[-]([\d])\4"
 
-    # this time, we will set inverse_match to true, because we wan't to check if the CPF is
+    # this time, we will set inverse_match to true,
+    # because we wan't to check if the CPF is
     # out of the range
     regex_validator = RegexValidator(
         regex=regex_code_all_equal,
@@ -41,8 +45,8 @@ def validate_cpf(value):
     cpf_calculated = cpf_numbers[:9]
 
     # now we do the math
-    # we need to multiply the first 9 numbers by 10 to 2 (from the first to the last)
-    # and we sum them
+    # we need to multiply the first 9 numbers
+    # by 10 to 2 (from the first to the last) and we sum them
     cpf_first_sum = cpf_second_sum = 0
     for i in range(0, 9):
         cpf_first_sum += cpf_numbers[i] * (10-i)
@@ -52,9 +56,9 @@ def validate_cpf(value):
     # and apply the rules
     cpf_calculated.append(calculate_cpf_verification_digit(cpf_first_sum))
 
-    # now we have the starting 9 digits and 1 calculated, so we have 10 digits on total
-    # to find the second verification digit we need to make the calculation with the first
-    # validation number added
+    # now we have the starting 9 digits and 1 calculated, so we have 10 digits
+    # on total to find the second verification digit we need to make the
+    # calculation with the first validation number added
     cpf_second_sum += cpf_numbers[9] * 2
 
     cpf_calculated.append(calculate_cpf_verification_digit(cpf_second_sum))
