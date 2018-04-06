61495235a6ccecfcecbcc8f9e7e36e9e42fbe9aa
Joberth Rogers <joberth.rogers18@gmail.com>
2018-04-06 18:36:28 -0300

    Make PEP8 revision by codeclimate
    Co-authored-by: Gabriela Medeiros <gabriela.medeiros2010@hotmail.com.br>

--- drdown/users/models/model_user.py
+++ drdown/users/models/model_user.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
-from drdown.utils.validators import validate_phone, validate_date
+from drdown.utils.validators import validate_phone
 import datetime
 
 class User(AbstractUser):
