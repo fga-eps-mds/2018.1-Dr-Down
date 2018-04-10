25f3dfe345d62c6d443aa392ce09ceea86c60fd5
Joberth Rogers <joberth.rogers18@gmail.com>
2018-04-10 13:07:14 -0300

    Specification about commentaries
    Adding View Commentary
    
    A template for show all them
    
    and modify the url
    
    Co-authored-by: Geovana Ramos <geovannna.1998@gmail.com>

--- drdown/forum/urls.py
+++ drdown/forum/urls.py
@@ -4,6 +4,10 @@ from .views.view_post import PostListView
 from .views.view_post import PostCreateView
 from .views.view_post import PostDeleteView
 from .views.view_post import PostUpdateView
+from .views.view_commentary import CommentaryListView
+from .views.view_commentary import CommentaryCreateView
+from .views.view_commentary import CommentaryDeleteView
+
 
 app_name = 'forum'
 urlpatterns = [
@@ -31,5 +35,20 @@ urlpatterns = [
         regex=r'^categories/(?P<slug>[-\w]+)-(?P<pk>\d+)/posts/(?P<post_pk>\d+)/edit/$',
         view=PostUpdateView.as_view(),
         name='update_post'
+    ),
+    url(
+        regex=r'^categories/(?P<slug>[-\w]+)-(?P<pk>\d+)/posts/(?P<commentary_pk>\d+)/$',
+        view=CommentaryListView.as_view(),
+        name='list_commentary'
+    ),
+    url(
+        regex=r'^categories/(?P<slug>[-\w]+)-(?P<pk>\d+)/posts/(?P<post_pk>\d+)/new/$',
+        view=CommentaryCreateView.as_view(),
+        name='create_commentary'
+    ),
+    url(
+        regex=r'^categories/(?P<slug>[-\w]+)-(?P<pk>\d+)/posts/(?P<commentary_pk>\d+)/delete/$',
+        view=CommentaryDeleteView.as_view(),
+        name='delete_commentary'
     ),
 ]
---
+++ drdown/forum/views/view_commentary.py
@@ -0,0 +1,88 @@
+from ..models.model_commentary import Commentary
+from django.views.generic import ListView
+from django.views.generic import CreateView
+from django.views.generic import DeleteView
+from django.views.generic import UpdateView
+from django.urls import reverse_lazy
+from django.utils.text import slugify
+from datetime import datetime
+
+
+class CommentaryListView(ListView):
+    model = Commentary
+    slug_field = 'message'
+    slug_url_kwarg = 'message'
+
+    def get_context_data(self, **kwargs):
+        context = super(CommentaryListView, self).get_context_data(**kwargs)
+        context['category_pk'] = self.kwargs.get('pk')
+        context['category_slug'] = self.kwargs.get('slug')
+        return context
+
+
+class CommentaryCreateView(CreateView):
+    model = Commentary
+    template_name = 'forum/form_post.html'
+    fields = ['title', 'message']
+    success_url = reverse_lazy('forum:list_commentary')
+
+    def form_valid(self, form):
+
+        # Get category that post belongs to
+        form.instance.category = Commentary.objects.get(pk=self.kwargs.get('pk'))
+        form.instance.created_by = self.request.user
+        form.save()
+
+        # Save slug with post title
+        form.instance.slug = slugify(
+            str(form.instance.id) +
+            "-" +
+            form.instance.title
+        )
+        form.save()
+
+        return super(CommentaryCreateView, self).form_valid(form)
+
+
+class CommentaryDeleteView (DeleteView):
+    model = Commentary
+    success_url = reverse_lazy('forum:list_categories')
+
+    def get_object(self):
+        commentary = Commentary.objects.get(
+            pk=self.kwargs.get('commentary_pk')
+        )
+        return commentary
+
+    def get_success_url(self):
+        return super(CommentaryDeleteView, self).get_success_url()
+
+
+class CommentaryUpdateView(UpdateView):
+    model = Commentary
+    template_name = 'forum/form_commentary.html'
+    fields = ['message']
+    success_url = reverse_lazy('forum:list_post')
+
+    def get_object(self):
+        commentary = Commentary.objects.get(
+            pk=self.kwargs.get('commentary_pk')
+        )
+        return commentary
+
+    def form_valid(self, form):
+
+        # Get updated_at datetime
+        form.instance.updated_at = datetime.now()
+        form.save()
+
+        # Change slug to new title
+        form.instance.slug = slugify(
+            str(form.instance.id) +
+            "-" +
+            form.instance.title
+        )
+        form.save()
+        return super(CommentaryUpdateView, self).form_valid(form)
+
+
