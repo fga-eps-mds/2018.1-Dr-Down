6a55f8e26e171965656d8a659bef5732c55c1193
GeovanaRamos <geovannna.1998@gmail.com>
2018-04-10 08:43:01 -0300

    Adding CreateView for posts
    Adding view, template and url for New Posts
    
    Adding link to create new posts
    
    Co-authored-by: joberth rogers <joberth.rogers18@gmail.com>

--- drdown/forum/urls.py
+++ drdown/forum/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from .views.view_category import CategoryListView
 from .views.view_post import PostListView
+from .views.view_post import PostCreateView
 
 app_name = 'forum'
 urlpatterns = [
@@ -13,5 +14,10 @@ urlpatterns = [
         regex=r'^categories/(?P<slug>[-\w]+)-(?P<pk>\d+)/$',
         view=PostListView.as_view(),
         name='list_posts'
+    ),
+    url(
+        regex=r'^categories/(?P<slug>[-\w]+)-(?P<pk>\d+)/new/$',
+        view=PostCreateView.as_view(),
+        name='create_post'
     ),
 ]
--- drdown/forum/views/view_post.py
+++ drdown/forum/views/view_post.py
@@ -1,10 +1,46 @@
 from ..models.model_post import Post
+from ..models.model_category import Category
 from django.views.generic import ListView
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from django.utils.text import slugify
 
 
 class PostListView(ListView):
     model = Post
     slug_field = 'title'
     slug_url_kwarg = 'title'
+
+    def get_context_data(self, **kwargs):
+        context = super(PostListView, self).get_context_data(**kwargs)
+        context['category_pk'] = self.kwargs.get('pk')
+        context['category_slug'] = self.kwargs.get('slug')
+        return context
+
+
+class PostCreateView(CreateView):
+    model = Post
+    template_name = 'forum/form_post.html'
+    fields = ['title', 'message']
+    success_url = reverse_lazy('forum:list_categories')
+
+    def form_valid(self, form):
+
+        # Get category that post belongs to
+        form.instance.category = Category.objects.get(pk=self.kwargs.get('pk'))
+        form.instance.created_by = self.request.user
+        form.save()
+
+        # Save slug with post title
+        form.instance.slug = slugify(
+            str(form.instance.id) +
+            "-" +
+            form.instance.title
+        )
+        form.save()
+
+        return super(PostCreateView, self).form_valid(form)
+
+
 
 
