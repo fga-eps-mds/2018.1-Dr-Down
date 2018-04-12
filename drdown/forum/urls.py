9628d0b847a834983acf24a909cf3b99ffb70af6
GeovanaRamos <geovannna.1998@gmail.com>
2018-04-12 09:25:45 -0300

    Shortening urls
    Removing "categories" from every url
    
    Co-authored-by: joberth rogers <joberth.rogers18@gmail.com>

--- drdown/forum/urls.py
+++ drdown/forum/urls.py
@@ -12,42 +12,42 @@ urlpatterns = [
         name='list_categories'
     ),
     url(
-        regex=r'^categories/(?P<slug>[-\w]+)-(?P<pk>\d+)/$',
+        regex=r'^(?P<slug>[-\w]+)-(?P<pk>\d+)/$',
         view=view_post.PostListView.as_view(),
         name='list_posts'
     ),
     url(
-        regex=r'^categories/(?P<slug>[-\w]+)-(?P<pk>\d+)/new/$',
+        regex=r'^(?P<slug>[-\w]+)-(?P<pk>\d+)/new/$',
         view=view_post.PostCreateView.as_view(),
         name='create_post'
     ),
     url(
-        regex=r'^categories/(?P<slug>[-\w]+)-(?P<pk>\d+)/posts/delete/(?P<post_pk>\d+)/$',
+        regex=r'^(?P<slug>[-\w]+)-(?P<pk>\d+)/posts/delete/(?P<post_pk>\d+)/$',
         view=view_post.PostDeleteView.as_view(),
         name='delete_post'
     ),
     url(
-        regex=r'^categories/(?P<slug>[-\w]+)-(?P<pk>\d+)/posts/edit/(?P<post_pk>\d+)/$',
+        regex=r'^(?P<slug>[-\w]+)-(?P<pk>\d+)/posts/edit/(?P<post_pk>\d+)/$',
         view=view_post.PostUpdateView.as_view(),
         name='update_post'
     ),
     url(
-        regex=r'^categories/(?P<slug>[-\w]+)-(?P<pk>\d+)/posts/(?P<post_pk>\d+)/$',
+        regex=r'^(?P<slug>[-\w]+)-(?P<pk>\d+)/posts/(?P<post_pk>\d+)/$',
         view=view_commentary.CommentaryListView.as_view(),
         name='list_commentary'
     ),
     url(
-        regex=r'^categories/(?P<slug>[-\w]+)-(?P<pk>\d+)/posts/(?P<post_pk>\d+)/new/$',
+        regex=r'^(?P<slug>[-\w]+)-(?P<pk>\d+)/posts/(?P<post_pk>\d+)/new/$',
         view=view_commentary.CommentaryCreateView.as_view(),
         name='create_commentary'
     ),
     url(
-        regex=r'^categories/(?P<slug>[-\w]+)-(?P<pk>\d+)/posts/(?P<post_pk>\d+)/delete/(?P<commentary_pk>\d+)/$',
+        regex=r'^(?P<slug>[-\w]+)-(?P<pk>\d+)/posts/(?P<post_pk>\d+)/delete/(?P<commentary_pk>\d+)/$',
         view=view_commentary.CommentaryDeleteView.as_view(),
         name='delete_commentary'
     ),
     url(
-        regex=r'^categories/(?P<slug>[-\w]+)-(?P<pk>\d+)/posts/(?P<post_pk>\d+)/update/(?P<commentary_pk>\d+)/$',
+        regex=r'^(?P<slug>[-\w]+)-(?P<pk>\d+)/posts/(?P<post_pk>\d+)/update/(?P<commentary_pk>\d+)/$',
         view=view_commentary.CommentaryUpdateView.as_view(),
         name='update_commentary'
     ),
