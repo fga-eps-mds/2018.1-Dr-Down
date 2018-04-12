0ebd77f8a79d05a996de44a14d71ce6ebabe5fc1
Joberth Rogers <joberth.rogers18@gmail.com>
2018-04-11 23:15:22 -0300

    Test for commentary view
    Co-authored-by: Geovanna Ramos <geovannna.1998@gmail.com>

---
+++ drdown/forum/tests/test_view_commentary.py
@@ -0,0 +1,65 @@
+from test_plus.test import TestCase
+from django.test.client import Client
+from ..models.model_category import Category
+from ..models.model_post import Post
+from ..models.model_commentary import Commentary
+from django.urls import reverse, resolve
+
+
+class TestViewPost(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.user = self.make_user()
+        self.category = Category.objects.create(
+            name="Test Category",
+            description="Making a category for post test case",
+            slug="test-slug"
+        )
+        self.post = Post.objects.create(
+            title="Test Post",
+            message="Making a post test case",
+            created_by=self.user,
+            category=self.category,
+        )
+
+        self.commentary = Commentary.objects.create(
+            message='abcde',
+            post=self.post,
+            updated_at='2018-06-09',
+            created_by=self.user,
+            updated_by=self.user,
+            slug='test',
+        )
+
+        self.url = "{% url 'forum:list_commentary' self.category.slug self.category.pk self.post.pk%}"
+        self.commentary.save()
+
+    def test_commentary_list_view(self):
+        """
+        Makes sure that the post list view is loaded correctly
+        """
+        response = self.client.get(
+            reverse('forum:list_commentary', args=(self.category.slug, self.category.pk, self.post.pk)))
+        self.assertEquals(response.status_code, 200)
+
+    def test_commentary_create_view(self):
+        """
+        Makes sure that the post create view is loaded correctly
+        """
+        response = self.client.get(reverse('forum:create_commentary', args=(self.category.slug, self.category.pk, self.post.pk)))
+        self.assertEquals(response.status_code, 200)
+
+    def test_commentary_update_view(self):
+        """
+        Makes sure that the post update view is loaded correctly
+        """
+        response = self.client.get(reverse('forum:update_commentary', args=(self.category.slug, self.category.pk, self.post.pk, self.commentary.pk)))
+        self.assertEquals(response.status_code, 200)
+
+    def test_commentary_delete_view(self):
+        """
+        Makes sure that the post update view is loaded correctly
+        """
+        response = self.client.get(reverse('forum:delete_commentary', args=(self.category.slug, self.category.pk, self.post.pk, self.commentary.pk)))
+        self.assertEquals(response.status_code, 200)
