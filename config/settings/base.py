dd74b5f8ad790ec0b74402209f741eb15760c4fa
Gabriela Medeiros <gabriela.medeiros2010@hotmail.com.br>
2018-04-16 15:14:17 -0300

    Applying translation
    Editing settings file
    
    Co-authored-by: Daniel Maike <danmke@hotmail.com>

--- config/settings/base.py
+++ config/settings/base.py
@@ -25,7 +25,8 @@ DEBUG = env.bool('DJANGO_DEBUG', False)
 # In Windows, this must be set to your system time zone.
 TIME_ZONE = 'America/Sao_Paulo'
 # https://docs.djangoproject.com/en/dev/ref/settings/#language-code
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
+LOCALE_PATHS = ( str(ROOT_DIR.path('locale')), )
 # https://docs.djangoproject.com/en/dev/ref/settings/#site-id
 SITE_ID = 1
 # https://docs.djangoproject.com/en/dev/ref/settings/#use-i18n
