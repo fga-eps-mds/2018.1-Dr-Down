7e7ad9940866a9e519d39003fa7055145a95cad1
Daniel Maike <danmke@hotmail.com>
2018-04-18 17:31:22 -0300

    Fixing PEP8 errors
    Co-authored-by: Gabriela Medeiros <gabriela.medeiros2010@hotmail.com.br>

--- drdown/users/admin.py
+++ drdown/users/admin.py
@@ -41,7 +41,8 @@ class MyUserAdmin(AuthUserAdmin):
     form = MyUserChangeForm
     add_form = MyUserCreationForm
     fieldsets = (
-            ('User Profile', {'fields': (('name'), ('gender'), ('telephone'), ('birthday'), ('photo'))}),
+            ('User Profile', {'fields': (('name'), ('gender'),
+             ('telephone'), ('birthday'), ('photo'))}),
     ) + AuthUserAdmin.fieldsets
     list_display = ('username', 'has_specialization', 'name', 'is_superuser')
     search_fields = ['name']
