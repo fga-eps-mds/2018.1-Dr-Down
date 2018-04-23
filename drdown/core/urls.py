b9156491ca96690a8e1627f51a55d017c488f057
GeovanaRamos <geovannna.1998@gmail.com>
2018-04-23 12:26:28 -0300

    Correcting PEP8 errors
    Co-authored-by:  Gabriela Medeiros <gabriela.medeiros2010@hotmail.com.br>

--- drdown/core/urls.py
+++ drdown/core/urls.py
@@ -4,6 +4,14 @@ from django.views.generic import TemplateView
 
 app_name = 'core'
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='core/home.html'), name='home'),
-    url(r'^info/$', TemplateView.as_view(template_name='core/info.html'), name='about'),
+    url(
+        regex=r'^$',
+        view=TemplateView.as_view(template_name='core/home.html'),
+        name='home'
+    ),
+    url(
+        regex=r'^info/$',
+        view=TemplateView.as_view(template_name='core/info.html'),
+        name='about'
+    ),
 ]
