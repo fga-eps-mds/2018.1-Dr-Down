8f5cf77d1167175ad8dade8cc46a76f8dc6a30e9
Elias Bernardo <ebmm01@gmail.com>
2018-04-23 20:55:35 -0300

    Adding MedicalRecords ListView
    Co-authored-by: joberth rogers <joberth.rogers18@gmail.com>

--- drdown/medicalrecords/views/view_medical_record.py
+++ drdown/medicalrecords/views/view_medical_record.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from ..models.model_medical_record import MedicalRecord
+from django.views.generic import ListView
+from django.views.generic import CreateView
+from django.views.generic import DeleteView
+from django.views.generic import UpdateView
+from django.urls import reverse_lazy
+from django.utils import timezone
 
-# Create your views here.
+
+class MedicalRecordsListView(ListView):
+    model = MedicalRecord
+
+# def get_context_data(self, **kwargs):
+#     context = super(MedicalRecordsListView, self).get_context_data(**kwargs)
+#     context['post'] = Post.objects.get(pk=self.kwargs.get('post_pk'))
+#     return context
+
+# def get_queryset(self):
+#    post = Post.objects.get(pk=self.kwargs.get('post_pk'))
+#    queryset = Commentary.objects.filter(post=post)
+#    return queryset
