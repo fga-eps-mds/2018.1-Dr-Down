f807ae8e5cde9a5d569a78d17e8f2d3e35863c76
Joberth Rogers <joberth.rogers18@gmail.com>
2018-04-24 20:19:26 -0300

    Modifies in url record
    Adding template medical record list
    
    URL additions
    
    Co-authored-by: Elias Bernardo <ebmm01@gmail.com>

--- drdown/medicalrecords/urls.py
+++ drdown/medicalrecords/urls.py
@@ -3,15 +3,20 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from drdown.medicalrecords.views import view_medical_record
+from drdown.users.views import view_user
 
 app_name = 'medicalrecords'
 urlpatterns = [
     url(
-        regex=r'^$',
+        regex=r'^(?P<username>[\w.@+-]+)$',
         view=view_medical_record.MedicalRecordsListView.as_view(),
         name='list_medicalrecords'
     ),
-
+    url(
+        regex=r'^$',
+        view=view_medical_record.MedicalRecordsUserListView.as_view(template_name='medicalrecords/medicalrecord_list_user.html'),
+        name='list_medicalrecords_user'
+    ),
     url(
         regex=r'^new/$',
         view=view_medical_record.MedicalRecordsCreateView.as_view(),
