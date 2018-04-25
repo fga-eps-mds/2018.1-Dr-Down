10a4d64d3891fdba5fa6b5c300d6c425b1c7c023
Gabriela Medeiros <gabriela.medeiros2010@hotmail.com.br>
2018-04-25 14:35:52 -0300

    Correcting PEP8 errors
    Co-authored-by: Geovana Ramos <geovannna.1998@gmail.com>

--- drdown/users/adapters.py
+++ drdown/users/adapters.py
@@ -15,7 +15,10 @@ class AccountAdapter(DefaultAccountAdapter):
 
     def get_login_redirect_url(self, request):
         if request.user.name:
-            return reverse('users:detail', kwargs={'username': request.user.username})
+            return reverse(
+                        viewname='users:detail', 
+                        kwargs={'username': request.user.username}
+            )
         else:
             return reverse('users:update')
 
