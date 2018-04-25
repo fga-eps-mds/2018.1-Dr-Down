f9c44c18e2f5df29d986515d8406f8f334f54ed2
Daniel Maike <danmke@hotmail.com>
2018-04-25 02:00:03 +0000

    Adding carelines
    Co-authored-by: Guilherme Guy <guilhermeguy349@gmail.com>

--- drdown/careline/models/model_checklist.py
+++ drdown/careline/models/model_checklist.py
@@ -22,6 +22,210 @@ class Checklist(models.Model):
             Procedure.PROCEDURE_AGES_WHEN_NEEDED:
                 [],
         },
+        {
+            Procedure.PROCEDURE_DESCRIPTION:
+                _("Early stimulation"),
+            Procedure.PROCEDURE_AGES_REQUIRED:
+                [
+                    Procedure.AGE_NEWBORN,
+                    Procedure.AGE_SIX_MONTHS,
+                    Procedure.AGE_ONE_YEAR,
+                ],
+            Procedure.PROCEDURE_AGES_WHEN_NEEDED:
+                [
+                    Procedure.AGE_TWO_YEARS,
+                    Procedure.AGE_THREE_YEARS,
+                ],
+        },
+        {
+            Procedure.PROCEDURE_DESCRIPTION:
+                _("Psychosocial support to the" +
+                  "family (associations, references)"),
+            Procedure.PROCEDURE_AGES_REQUIRED:
+                Procedure.AGES,
+            Procedure.PROCEDURE_AGES_WHEN_NEEDED:
+                [],
+        },
+        {
+            Procedure.PROCEDURE_DESCRIPTION:
+                _("Physiotherapy"),
+            Procedure.PROCEDURE_AGES_REQUIRED:
+                [
+                    Procedure.AGE_NEWBORN,
+                    Procedure.AGE_SIX_MONTHS,
+                    Procedure.AGE_ONE_YEAR,
+                ],
+            Procedure.PROCEDURE_AGES_WHEN_NEEDED:
+                [
+                    Procedure.AGE_TWO_YEARS,
+                    Procedure.AGE_THREE_YEARS,
+                    Procedure.AGE_FIVE_YEARS,
+                    Procedure.AGE_SIX_TO_TEN_YEARS,
+                ],
+        },
+        {
+            Procedure.PROCEDURE_DESCRIPTION:
+                _("Speech Therapy"),
+            Procedure.PROCEDURE_AGES_REQUIRED:
+                Procedure.AGES,
+            Procedure.PROCEDURE_AGES_WHEN_NEEDED:
+                [],
+        },
+        {
+            Procedure.PROCEDURE_DESCRIPTION:
+                _("Cardiologic (echocardiogram, electrocardiogram)"),
+            Procedure.PROCEDURE_AGES_REQUIRED:
+                [
+                    Procedure.AGE_NEWBORN,
+                ],
+            Procedure.PROCEDURE_AGES_WHEN_NEEDED:
+                [
+                    Procedure.AGE_ONE_YEAR,
+                    Procedure.AGE_TWO_YEARS,
+                    Procedure.AGE_THREE_YEARS,
+                    Procedure.AGE_FIVE_YEARS,
+                    Procedure.AGE_SIX_TO_TEN_YEARS,
+                ],
+        },
+        {
+            Procedure.PROCEDURE_DESCRIPTION:
+                _("Genetics (karyotype, counseling)"),
+            Procedure.PROCEDURE_AGES_REQUIRED:
+                [
+                    Procedure.AGE_NEWBORN,
+                ],
+            Procedure.PROCEDURE_AGES_WHEN_NEEDED:
+                [],
+        },
+        {
+            Procedure.PROCEDURE_DESCRIPTION:
+                _("Audiological (otoacoustic emissions, PEATE)"),
+            Procedure.PROCEDURE_AGES_REQUIRED:
+                [
+                    Procedure.AGE_NEWBORN,
+                ],
+            Procedure.PROCEDURE_AGES_WHEN_NEEDED:
+                [],
+        },
+        {
+            Procedure.PROCEDURE_DESCRIPTION:
+                _("Audiological (audio/impedanciometry, PEATE) +" +
+                  "otolaryngologist"),
+            Procedure.PROCEDURE_AGES_REQUIRED:
+                [
+                    Procedure.AGE_SIX_MONTHS,
+                    Procedure.AGE_ONE_YEAR,
+                    Procedure.AGE_TWO_YEARS,
+                    Procedure.AGE_THREE_YEARS,
+                    Procedure.AGE_FIVE_YEARS,
+                    Procedure.AGE_SIX_TO_TEN_YEARS,
+                ],
+            Procedure.PROCEDURE_AGES_WHEN_NEEDED:
+                [],
+        },
+        {
+            Procedure.PROCEDURE_DESCRIPTION:
+                _("Dentistry"),
+            Procedure.PROCEDURE_AGES_REQUIRED:
+                [
+                    Procedure.AGE_SIX_MONTHS,
+                    Procedure.AGE_ONE_YEAR,
+                    Procedure.AGE_TWO_YEARS,
+                    Procedure.AGE_THREE_YEARS,
+                    Procedure.AGE_FIVE_YEARS,
+                    Procedure.AGE_SIX_TO_TEN_YEARS,
+                ],
+            Procedure.PROCEDURE_AGES_WHEN_NEEDED:
+                [
+                    Procedure.AGE_NEWBORN,
+                ],
+        },
+        {
+            Procedure.PROCEDURE_DESCRIPTION:
+                _("Ophthalmologic"),
+            Procedure.PROCEDURE_AGES_REQUIRED:
+                [
+                    Procedure.AGE_NEWBORN,
+                    Procedure.AGE_ONE_YEAR,
+                    Procedure.AGE_TWO_YEARS,
+                    Procedure.AGE_THREE_YEARS,
+                    Procedure.AGE_FIVE_YEARS,
+                    Procedure.AGE_SIX_TO_TEN_YEARS,
+                ],
+            Procedure.PROCEDURE_AGES_WHEN_NEEDED:
+                [],
+        },
+        {
+            Procedure.PROCEDURE_DESCRIPTION:
+                _("Other therapies (occupational therapy, swimming," +
+                  "psychomotricity, psychopedagogy, hydro/equo/music" +
+                  "therapy, arts, others)"),
+            Procedure.PROCEDURE_AGES_REQUIRED:
+                [
+                    Procedure.AGE_SIX_MONTHS,
+                    Procedure.AGE_ONE_YEAR,
+                    Procedure.AGE_TWO_YEARS,
+                    Procedure.AGE_THREE_YEARS,
+                    Procedure.AGE_FIVE_YEARS,
+                    Procedure.AGE_SIX_TO_TEN_YEARS,
+                ],
+            Procedure.PROCEDURE_AGES_WHEN_NEEDED:
+                [],
+        },
+        {
+            Procedure.PROCEDURE_DESCRIPTION:
+                _("Abdominal ultrasound"),
+            Procedure.PROCEDURE_AGES_REQUIRED:
+                [
+                    Procedure.AGE_NEWBORN,
+                    Procedure.AGE_TWO_YEARS,
+                    Procedure.AGE_FIVE_YEARS,
+                    Procedure.AGE_SIX_TO_TEN_YEARS,
+                ],
+            Procedure.PROCEDURE_AGES_WHEN_NEEDED:
+                [
+                    Procedure.AGE_THREE_YEARS,
+                ],
+        },
+        {
+            Procedure.PROCEDURE_DESCRIPTION:
+                _("Atlantoaxial X-ray * (extension, flexion and neutral)"),
+            Procedure.PROCEDURE_AGES_REQUIRED:
+                [
+                    Procedure.AGE_TWO_YEARS,
+                    Procedure.AGE_FIVE_YEARS,
+                ],
+            Procedure.PROCEDURE_AGES_WHEN_NEEDED:
+                [],
+        },
+        {
+            Procedure.PROCEDURE_DESCRIPTION:
+                _("Laboratory tests ** (blood count, thyrogram," +
+                  "blood glucose and lipidogram - if diabetes/obesity" +
+                  "is suspected)"),
+            Procedure.PROCEDURE_AGES_REQUIRED:
+                [
+                    Procedure.AGE_NEWBORN,
+                    Procedure.AGE_ONE_YEAR,
+                    Procedure.AGE_TWO_YEARS,
+                    Procedure.AGE_THREE_YEARS,
+                    Procedure.AGE_FIVE_YEARS,
+                    Procedure.AGE_SIX_TO_TEN_YEARS,
+                ],
+            Procedure.PROCEDURE_AGES_WHEN_NEEDED:
+                [],
+        },
+        {
+            Procedure.PROCEDURE_DESCRIPTION:
+                _("Research for Celiac Disease (antigliadin antibody)"),
+            Procedure.PROCEDURE_AGES_REQUIRED:
+                [
+                    Procedure.AGE_NEWBORN,
+                    Procedure.AGE_TWO_YEARS,
+                ],
+            Procedure.PROCEDURE_AGES_WHEN_NEEDED:
+                [],
+        },
     ]
 
     patient = models.OneToOneField(
@@ -55,4 +259,4 @@ def create_procedures(sender, instance, **kwargs):
         )
 
     instance.initialized = True
-    instance.save() # only called once, so there is no recursion error
+    instance.save()  # only called once, so there is no recursion error
