23f4c26ae09ef3e1093e9bbca145dc7f30f7954d
João Pedro Sconetto <sconetto.joao@gmail.com>
2018-04-26 14:49:42 -0300

    Working on mkdocs CI build
    Add subfolders of build to git ignore
    
    Modification on .travis.yml
    
    Add software pdf doc into docs/ folder
    
    Minor corrections onto mkdocs shell script
    
    Co-authored-by: Mariana Mendes <mare.s.mendes@gmail.com>

--- mkdocs-build.sh
+++ mkdocs-build.sh
@@ -1,25 +1,11 @@
 #!/usr/bin/env bash
 set -o errexit -o nounset
 
-# Get curent commit revision
-rev=$(git rev-parse --short HEAD)
+# Configure the git inside of Travis Machine
 
-# Initialize gh-pages checkout
-(
-    cd docs/
-    git config user.name "${GH_USER_NAME}"
-    git config user.email "${GH_USER_EMAIL}"
-    git remote add upstream "https://${GH_TOKEN}@${GH_REF}"
-    git fetch upstream
-    git reset upstream/gh-pages
-)
-
-# Commit and push the documentation to gh-pages
-(
-    touch .
-    git add -A .
-    git commit -m "Deployed ${rev} with MkDocs version: 0.17.3"
-    git push -q upstream HEAD:gh-pages
-)
+git config user.name "${GH_USER_NAME}"
+git config user.email "${GH_USER_EMAIL}"
+git remote add upstream "https://${GH_TOKEN}@${GH_REF}"
+git fetch upstream
 
 mkdocs build --clean
