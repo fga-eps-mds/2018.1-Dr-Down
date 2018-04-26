e2ce59e02a95e1d8199822d6195dba95aca8022d
João Pedro Sconetto <sconetto.joao@gmail.com>
2018-04-26 15:40:43 -0300

    Last fixes
    Reorganizing the travis steps
    
    Adding commit and push to the pdf document
    
    Co-authored-by: Mariana Mendes <mare.s.mendes@gmail.com>

--- mkdocs-build.sh
+++ mkdocs-build.sh
@@ -1,11 +1,23 @@
 #!/usr/bin/env bash
 set -o errexit -o nounset
 
+# Get current commit revision
+rev=$(git rev-parse --short HEAD)
+
 # Configure the git inside of Travis Machine
+(
+  git config user.name "${GH_USER_NAME}"
+  git config user.email "${GH_USER_EMAIL}"
+  git remote set-url origin "https://${GH_TOKEN}@${GH_REF}"
+  git fetch origin
+)
 
-git config user.name "${GH_USER_NAME}"
-git config user.email "${GH_USER_EMAIL}"
-git remote add upstream "https://${GH_TOKEN}@${GH_REF}"
-git fetch upstream
+# Commit and push the sphinx documentation to develop
+(
+  git add docs/drdown.pdf
+  git commit -m "Rebuild sphinx documentation at ${rev} "
+  git push --force origin develop
+)
 
+# Running mkdocs build to gh-pages
 mkdocs build --clean
