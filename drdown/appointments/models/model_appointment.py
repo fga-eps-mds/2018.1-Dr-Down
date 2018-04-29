cce4d3bff2778085922cf6f066e25c15a73fc19c
GeovanaRamos <geovannna.1998@gmail.com>
2018-04-29 17:41:32 -0300

    Creating model Appointment
    Adding attributes, __string__ and class meta
    
    Adding model to admin and init
    
    Creating migration
    
    Co-authored-by:  Gabriela Medeiros <gabriela.medeiros2010@hotmail.com.br>

---
+++ drdown/appointments/models/model_appointment.py
@@ -0,0 +1,102 @@
+from django.db import models
+from drdown.users.models.model_health_team import HealthTeam
+from drdown.users.models.model_patient import Patient
+from django.utils.translation import ugettext_lazy as _
+
+
+class Appointment(models.Model):
+
+    MORNING = 'M'
+    AFTERNOON = 'A'
+
+    SHIFT_CHOICES = (
+        (MORNING, _('Morning')),
+        (AFTERNOON, _('Afternoon')),
+    )
+
+    shift = models.CharField(
+        _('Shift'),
+        choices=SHIFT_CHOICES,
+        help_text=_('Shift of appointment'),
+        max_length=10
+    )
+
+    date_time = models.DateTimeField(
+        _('Date and Time'),
+        help_text=_('Date and time of appointment'),
+        max_length=50
+    )
+
+    motive = models.TextField(
+        _('Motive'),
+        help_text=_('Why are you requesting an appointment?'),
+        max_length=500
+    )
+
+    SPEECH_THERAPHY = _("Speech Therapy")
+    PSYCHOLOGY = _("Psychology")
+    PHYSIOTHERAPY = _("Physiotherapy")
+    OCCUPATIONAL_THERAPY = _("Occupational Therapy")
+    DOCTOR = _("Doctor")
+    CARDIOLOGY = _("Cardiology")
+    NEUROLOGY = _("Neurology")
+    PEDIATRICS = _("Pediatrics")
+    NURSING = _("Nursing")
+
+    SPECIALITY_CHOICES = (
+        (SPEECH_THERAPHY, _('Speech Therapy')),
+        (PSYCHOLOGY, _('Psychology')),
+        (PHYSIOTHERAPY, _('Physiotherapy')),
+        (OCCUPATIONAL_THERAPY, _('Occupational Therapy')),
+        (DOCTOR, _('Doctor')),
+        (CARDIOLOGY, _('Cardiology')),
+        (NEUROLOGY, _('Neurology')),
+        (PEDIATRICS, _('Pediatrics')),
+        (NURSING, _('Nursing')),
+    )
+
+    speciality = models.CharField(
+        _('Speciality'),
+        choices=SPECIALITY_CHOICES,
+        help_text=_("Speciality of appointment"),
+        max_length=30
+    )
+
+    doctor = models.ForeignKey(
+        HealthTeam,
+        on_delete=models.CASCADE,
+        verbose_name=_('Doctor'),
+        related_name='appointments',
+    )
+
+    patient = models.ForeignKey(
+        Patient,
+        on_delete=models.CASCADE,
+        verbose_name=_('Patient'),
+        related_name='appointments',
+    )
+
+    SCHEDULED = _('Scheduled')
+    CANCELED = _('Canceled')
+    DONE = _('Done')
+
+    STATUS_CHOICES = (
+        (SCHEDULED, _('Scheduled')),
+        (CANCELED, _('Canceled')),
+        (DONE, _('Done'))
+    )
+
+    status = models.CharField(
+        _('Status'),
+        choices=STATUS_CHOICES,
+        help_text=_("Is the appointment still scheduled?"),
+        default=SCHEDULED,
+        max_length=20
+    )
+
+    def __str__(self):
+        return _('Appointment of ') + self.patient
+
+    class Meta:
+        verbose_name = _("Appointment")
+        verbose_name_plural = _("Appointments")
