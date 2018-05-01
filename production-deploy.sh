666d5d5d9bff1dd9d62591f9e7b23776db6344b4
João Pedro Sconetto <sconetto.joao@gmail.com>
2018-05-01 10:29:00 -0300

    Modifying deploy scripts
    Chaging docker image name because of error on travis
    
    Adding login onto .travis.yml instead of in the shell script
    
    Co-authored-by: Mariana Mendes <mare.s.mendes@gmail.com>

--- production-deploy.sh
+++ production-deploy.sh
@@ -4,8 +4,7 @@
 #
 # Author: João Pedro Sconetto <sconetto.joao@gmail.com>
 
-echo $DOCKER_ID_USER_PASSWORD | docker login --username $DOCKER_ID_USER --password-stdin
-docker tag 20181drdown_django $DOCKER_ID_USER/20181-dr-down_django
+docker tag 20181drdown_django_1 $DOCKER_ID_USER/20181-dr-down_django
 docker push $DOCKER_ID_USER/20181-dr-down_django
 
 sudo apt-get install sshpass -y
