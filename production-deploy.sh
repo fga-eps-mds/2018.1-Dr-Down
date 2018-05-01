c5f1961ed1e8464388f60e34420b013ad87cfba2
João Pedro Sconetto <sconetto.joao@gmail.com>
2018-05-01 11:11:37 -0300

    Changing docker tag name
    Chaging name on docker tag to fix deploy error
    
    Co-authored-by: Mariana Mendes <mare.s.mendes@gmail.com>

--- production-deploy.sh
+++ production-deploy.sh
@@ -4,7 +4,7 @@
 #
 # Author: João Pedro Sconetto <sconetto.joao@gmail.com>
 
-docker tag 20181drdown_django_1 $DOCKER_ID_USER/20181-dr-down_django
+docker tag 20181-dr-down_django:latest $DOCKER_ID_USER/20181-dr-down_django
 docker push $DOCKER_ID_USER/20181-dr-down_django
 
 sudo apt-get install sshpass -y
