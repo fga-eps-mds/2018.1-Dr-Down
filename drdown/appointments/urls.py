2aaf2aa6bfebdf404c32561f0118ad9168722218
Gabriela Medeiros <gabriela.medeiros2010@hotmail.com.br>
2018-05-01 12:20:55 -0300

    Adding functionality create appointment
    Adding new appointment url and view
    
    Co-authored-by: Geovana Ramos <geovannna.1998@gmail.com>

--- drdown/appointments/urls.py
+++ drdown/appointments/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from drdown.appointments.views.view_appointment import AppointmentListView
+from drdown.appointments.views.view_appointment import AppointmentCreateView
 
 app_name = 'appointments'
 urlpatterns = [
@@ -8,4 +9,10 @@ urlpatterns = [
         view=AppointmentListView.as_view(),
         name='list_appointments'
     ),
+    url(
+        regex=r'^new/$',
+        view=AppointmentCreateView.as_view(),
+        name='create_appointment'
+    ),
+
 ]
--- drdown/appointments/views/view_appointment.py
+++ drdown/appointments/views/view_appointment.py
@@ -3,6 +3,7 @@ from django.views.generic import CreateView
 from django.views.generic import UpdateView
 from drdown.appointments.models import Appointment
 from django.utils import timezone
+from django.urls import reverse
 
 
 class AppointmentListView(ListView):
@@ -27,7 +28,7 @@ class AppointmentListView(ListView):
         if first == last:
             years.append(first)
         else:
-            for year in range(first, last+1):
+            for year in range(first, last + 1):
                 years.append(year)
         return years
 
@@ -53,3 +54,20 @@ class AppointmentListView(ListView):
         context['current_year'] = timezone.now().year
         return context
 
+
+class AppointmentCreateView(CreateView):
+    model = Appointment
+    template_name = 'appointments/form_appointment.html'
+    fields = ['speciality',
+              'shift',
+              'doctor',
+              'patient',
+              'date_time',
+              'motive', ]
+
+    def get_success_url(self, **kwargs):
+        success_create_url = reverse(
+            viewname='appointments:list_appointments',
+        )
+
+        return success_create_url
