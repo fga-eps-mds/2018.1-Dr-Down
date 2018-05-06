8eaf8b9202ffc0b9be2ec3a17a283e49865e0a94
GeovanaRamos <geovannna.1998@gmail.com>
2018-05-06 18:25:34 -0300

    Adding tests for appointment requests
    Adding tests to cover 100% of model
    
    Co-authored-by: Daniel Maike <danmke@hotmail.com>

---
+++ drdown/appointments/tests/test_model_request.py
@@ -0,0 +1,133 @@
+from test_plus.test import TestCase
+from ..models.model_request import Request
+from drdown.users.models.model_health_team import HealthTeam
+from drdown.users.models.model_patient import Patient
+from django.utils.translation import ugettext_lazy as _
+
+
+class TestModelRequest(TestCase):
+
+    def setUp(self):
+        """
+            This method will run before any test case.
+        """
+
+        self.user = self.make_user(username='user_1')
+        self.user2 = self.make_user(username='user_2')
+        self.patient = Patient.objects.create(
+            ses="1234567",
+            user=self.user,
+            priority=1,
+            mother_name="Mother",
+            father_name="Father",
+            ethnicity=3,
+            sus_number="12345678911",
+            civil_registry_of_birth="12345678911",
+            declaration_of_live_birth="12345678911"
+        )
+
+        self.doctor = HealthTeam.objects.create(
+            cpf="057.641.271-65",
+            user=self.user2,
+            speciality=HealthTeam.NEUROLOGY,
+            council_acronym=HealthTeam.CRM,
+            register_number="1234567",
+            registration_state=HealthTeam.DF,
+        )
+
+        self.request = Request.objects.create(
+            motive='Some motive',
+            observation='Some obs',
+            speciality=Request.SPEECH_THERAPHY,
+            doctor=self.doctor,
+            patient=self.patient,
+            status=Request.SCHEDULED
+        )
+
+    def test_one_to_one_relation(self):
+        """
+            Test to verify if the relations work
+        """
+
+        self.assertIs(self.patient, self.request.patient)
+        self.assertIs(self.doctor, self.request.doctor)
+
+    def test_delete_cascade_patient(self):
+        """
+            Verify delete the relation objects
+        """
+
+        self.patient.delete()
+        with self.assertRaises(Request.DoesNotExist):
+            Request.objects.get()
+
+    def test_delete_cascade_doctor(self):
+        """
+            Verify delete the relation objects
+        """
+
+        self.doctor.delete()
+        with self.assertRaises(Request.DoesNotExist):
+            Request.objects.get()
+
+    def test_save_patient_ok(self):
+        """
+        Test to verify if patient is correctly passed
+        """
+
+        self.assertEquals(self.request.patient, self.patient)
+
+    def test_save_doctor_ok(self):
+        """
+        Test to verify if doctor is correctly passed
+        """
+
+        self.assertEquals(self.request.doctor, self.doctor)
+
+    def test_save_status_ok(self):
+        """
+        Test to verify if status is correctly passed
+        """
+
+        self.assertEquals(self.request.status, Request.SCHEDULED)
+
+    def test_save_speciality_ok(self):
+        """
+        Test to verify if speciality is correctly passed
+        """
+
+        self.assertEquals(
+            self.request.speciality,
+            Request.SPEECH_THERAPHY
+        )
+
+    def test_save_motive_ok(self):
+        """
+        Test to verify if motive is correctly passed
+        """
+
+        self.assertEquals(
+            self.request.motive,
+            'Some motive'
+        )
+
+    def test_save_observation_ok(self):
+        """
+        Test to verify if observation is correctly passed
+        """
+
+        self.assertEquals(
+            self.request.observation,
+            'Some obs'
+        )
+
+
+    def test_str_is_equal_to_title(self):
+        """
+        Test if method `__str__` is returning "Request of <patient>"
+        """
+        self.user.name = 'User'
+        self.assertEqual(
+            str(self.request),
+            _('Appointment request of ') + self.patient.user.name
+        )
