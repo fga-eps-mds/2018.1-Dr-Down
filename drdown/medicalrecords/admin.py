2673b444e9f8a2f4739b07f9c943cddb41d6df3d
Elias Bernardo <ebmm01@gmail.com>
2018-05-07 10:22:10 -0300

    Creating model Specific Exams
    Adding Specific Exams atributes
    
    Adding Specific Exams to Django Admin
    
    Co-authored-by: Guilherme Guy <guilhermeguy349@gmail.com>

--- drdown/medicalrecords/admin.py
+++ drdown/medicalrecords/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from .models.model_medical_record import MedicalRecord
 from .models.model_static_data import StaticData
 from .models.model_medicines import Medicine
+from .models.model_specific_exams import SpecificExam
 
 admin.site.register(MedicalRecord)
 admin.site.register(StaticData)
 admin.site.register(Medicine)
+admin.site.register(SpecificExam)
---
+++ drdown/medicalrecords/models/model_specific_exams.py
@@ -0,0 +1,59 @@
+from django.db import models
+from drdown.users.models.model_health_team import HealthTeam
+from drdown.users.models.model_patient import Patient
+from django.utils.translation import ugettext_lazy as _
+
+
+class SpecificExam(models.Model):
+
+    patient = models.OneToOneField(
+        Patient,
+        on_delete=models.CASCADE,
+        verbose_name=_('Patient')
+    )
+
+    structured_physical_exam = models.FileField(
+        upload_to='media/medicalrecords/specificexams',
+        blank=True,
+        verbose_name=_('Structured Physical Exam')
+    )
+
+    vision = models.FileField(
+        upload_to='media/medicalrecords/specificexams',
+        blank=True,
+        verbose_name=_('Vision Exam')
+    )
+
+    ear = models.FileField(
+        upload_to='media/medicalrecords/specificexams',
+        blank=True,
+        verbose_name=_('Ear Exam')
+    )
+
+    hearth = models.FileField(
+        upload_to='media/medicalrecords/specificexams',
+        blank=True,
+        verbose_name=_('Hearth Exam')
+    )
+
+    muscle_skeletal_system = models.FileField(
+        upload_to='media/medicalrecords/specificexams',
+        blank=True,
+        verbose_name=_('Muscle skeletal system Exam')
+    )
+
+    nervous_system = models.FileField(
+        upload_to='media/medicalrecords/specificexams',
+        blank=True,
+        verbose_name=_('Nervous system Exam')
+    )
+
+    author = models.ForeignKey(
+        HealthTeam,
+        on_delete=models.CASCADE,
+        verbose_name=_("Author")
+    )
+
+    class Meta:
+        verbose_name = _("Specific Exam")
+        verbose_name_plural = _("Specific Exams")
