ad0ba0fb85b6e567e09a3c6f51138a2d6bd105e4
Daniel Maike <danmke@hotmail.com>
2018-05-07 16:20:06 -0300

    Implementing view_request
    Adding RequestCreateView
    
    Adding RequestUpdateView
    
    Co-authored-by: Geovana Ramos <geovannna.1998@gmail.com>

--- drdown/appointments/views/view_request.py
+++ drdown/appointments/views/view_request.py
@@ -28,3 +28,67 @@ class RequestListView(LoginRequiredMixin, SearchListView):
     form_class = RequestSearchForm
     filter_class = RequestFilter
     paginate_by = 10
+
+    @staticmethod
+    def prepare_queryset(request):
+        user = request.user
+        if hasattr(user, 'patient'):
+            queryset = Request.objects.filter(
+                patient=user.patient
+            ).order_by('id')
+        elif hasattr(user, 'responsible'):
+            queryset = Request.objects.filter(
+                patient__in=user.responsible.patient_set.all()
+            ).order_by('id')
+        elif hasattr(user, 'employee'):
+            queryset = Request.objects.all(
+            ).order_by('id')
+        else:
+            queryset = Request.objects.none()
+        return queryset
+
+    def get_queryset(self):
+        return self.prepare_queryset(self.request)
+
+
+class RequestCreateView(LoginRequiredMixin, CreateView):
+    model = Request
+    template_name = 'appointments/request_form.html'
+    fields = [
+        'speciality',
+        'doctor',
+        'patient',
+        'shift',
+        'day',
+    ]
+
+    def get_success_url(self, **kwargs):
+        success_create_url = reverse(
+            viewname='appointments:list_requests',
+        )
+
+        return success_create_url
+
+
+class RequestUpdateView(LoginRequiredMixin, UpdateView):
+    model = Request
+    template_name = 'appointments/request_form.html'
+    fields = ['speciality',
+              'doctor',
+              'patient',
+              'shift',
+              'day',
+    ]
+
+    def get_success_url(self, **kwargs):
+        success_update_url = reverse(
+            viewname='appointments:list_requests',
+        )
+
+        return success_update_url
+
+    def get_object(self):
+        request = Request.objects.get(
+            pk=self.kwargs.get('request_pk')
+        )
+        return request
