d77f10a8583ef754383896af21a2aeb4113995e5
Elias Bernardo <ebmm01@gmail.com>
2018-05-06 21:31:42 -0300

    Adding model StaticData on medicalrecords app
    Adding the model with independent attributes of a medical record
    
    Co-authored-by: Guilherme Guy <guilhermeguy349@gmail.com>

---
+++ drdown/medicalrecords/models/model_static_data.py
@@ -0,0 +1,46 @@
+from django.db import models
+from drdown.users.models.model_health_team import HealthTeam
+from drdown.users.models.model_patient import Patient
+from django.utils.translation import ugettext_lazy as _
+from datetime import datetime
+from django.core.exceptions import ValidationError
+
+
+class StaticData(models.Model):
+
+    patient = models.ForeignKey(
+        Patient,
+        on_delete=models.CASCADE,
+        verbose_name=_('Patient')
+    )
+
+    weight = models.IntegerField(
+        max_length=4,
+        verbose_name = _('Weight at birth'),
+        help_text = _('In grams')
+    )
+
+    APGAR = models.IntegerField(
+        choices=list(zip(range(1, 10), range(1, 10))),
+    )
+
+    heart_test = models.FileField(
+        upload_to='media/medicalrecords',
+        blank=True,
+        verbose_name = _('Heart Test')
+    )
+
+    ear_test = models.FileField(
+        upload_to='media/medicalrecords',
+        blank=True,
+        verbose_name = _('Test of the Ear')
+    )
+
+    author = models.ForeignKey(
+        HealthTeam,
+        on_delete=models.CASCADE,
+        verbose_name=_("Author")
+    )
+
+    class Meta:
+        verbose_name = _("Medical Record Static Data")
