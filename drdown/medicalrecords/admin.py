a5873a32128f32970297a88b7e7aa95406ece3bd
Elias Bernardo <ebmm01@gmail.com>
2018-05-07 11:22:34 -0300

    Creating model Complaint
    Adding Complaint to Django Admin
    
    Adding Complaint migration
    
    Co-authored-by: Guilherme Guy <guilhermeguy349@gmail.com>

--- drdown/medicalrecords/admin.py
+++ drdown/medicalrecords/admin.py
@@ -3,8 +3,10 @@ from .models.model_medical_record import MedicalRecord
 from .models.model_static_data import StaticData
 from .models.model_medicines import Medicine
 from .models.model_specific_exams import SpecificExam
+from .models.model_complaint import Complaint
 
 admin.site.register(MedicalRecord)
 admin.site.register(StaticData)
 admin.site.register(Medicine)
 admin.site.register(SpecificExam)
+admin.site.register(Complaint)
---
+++ drdown/medicalrecords/models/model_complaint.py
@@ -0,0 +1,40 @@
+from django.db import models
+from drdown.users.models.model_health_team import HealthTeam
+from drdown.users.models.model_patient import Patient
+from django.utils.translation import ugettext_lazy as _
+from datetime import datetime
+
+
+class Complaint(models.Model):
+
+    created_at = models.DateTimeField(
+        _('Created at'),
+        help_text=_('Date of creation'),
+        auto_now_add=True
+    )
+
+    complaint_day = models.DateTimeField(
+        _('Day and time of complaint'),
+    )
+
+    patient = models.ForeignKey(
+        Patient,
+        on_delete=models.CASCADE,
+        verbose_name=_('Patient')
+    )
+
+    description = models.TextField(
+        _('Description'),
+        help_text=_('Description of the complaint'),
+        max_length=4000
+    )
+
+    author = models.ForeignKey(
+        HealthTeam,
+        on_delete=models.CASCADE,
+        verbose_name=_("Author")
+    )
+
+    class Meta:
+        verbose_name = _("Complaint")
+        verbose_name_plural = _("Complaints")
