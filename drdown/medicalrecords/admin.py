145ef5d0033cd7cbe499f59f5dee9533897247f3
Elias Bernardo <ebmm01@gmail.com>
2018-05-06 21:33:27 -0300

    Listing StaticData on DjangoAdmin
    Adding StaticData model to MedicalRecords admin.py
    
    Co-authored-by: Guilherme Guy <guilhermeguy349@gmail.com>

--- drdown/medicalrecords/admin.py
+++ drdown/medicalrecords/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from .models.model_medical_record import MedicalRecord
+from .models.model_static_data import StaticData
 
 admin.site.register(MedicalRecord)
+admin.site.register(StaticData)
