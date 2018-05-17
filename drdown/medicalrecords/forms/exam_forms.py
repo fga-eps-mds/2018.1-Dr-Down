920ef82745e102ee0172a0778c8083e72b99b38e
GeovanaRamos <geovannna.1998@gmail.com>
2018-05-17 12:51:42 -0300

    Correcting exams forms
    Correcting indents and line spacing on template
    
    Adding date format input
    
    Co-authored-by: joberth rogers <joberth.rogers18@gmail.com>
    
    Co-authored-by: Guilherme Guy <guilhermeguy349@gmail.com>

--- drdown/medicalrecords/forms/exam_forms.py
+++ drdown/medicalrecords/forms/exam_forms.py
@@ -7,3 +7,6 @@ class ExamForm(forms.ModelForm):
     class Meta:
         model = Exam
         fields = ["file", "day", "category", "observations"]
+        widgets = {
+            'day': forms.DateInput(attrs={'type': 'date'})
+        }
