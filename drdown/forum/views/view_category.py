39d5710eeee60c70efea0bc61a3fc8cb844b0258
Elias Bernardo <ebmm01@gmail.com>
2018-05-20 13:37:18 -0300

    Adding sidebar support to forum
    Creating view_base in Forum app
    
    Adding BaseViewTemplate in view_category, view_post and view_commentary
    
    Changing the extends argument for view.get_template in all Forum htmls
    
    Changing the delete template of posts and comments
    
    Co-authored-by: Geovana Ramos <geovannna.1998@gmail.com>

--- drdown/forum/views/view_category.py
+++ drdown/forum/views/view_category.py
@@ -1,8 +1,10 @@
 from ..models.model_category import Category
 from django.views.generic import ListView
+from ..views.view_base import BaseViewTemplate
 
 
-class CategoryListView(ListView):
+class CategoryListView(BaseViewTemplate, ListView):
     model = Category
     slug_field = 'name'
     slug_url_kwarg = 'name'
+
