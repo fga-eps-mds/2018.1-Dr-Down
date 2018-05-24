202588f55c9e2b253d01c6987d8e5d3e4d7c01c4
Joberth Rogers <joberth.rogers18@gmail.com>
2018-05-24 17:00:12 -0300

    Correting PEP8
    models risk, form risk
    
    Co-authored-by: Daniel Maike <danmke@hotmail.com>

--- drdown/medicalrecords/forms/risk_forms.py
+++ drdown/medicalrecords/forms/risk_forms.py
@@ -7,7 +7,6 @@ class RiskForm(forms.ModelForm):
     class Meta:
         model = Risk
         fields = ["priority_speech_theraphy", "priority_psychology",
-            "priority_physiotherapy", "priority_cardiology",
-            "priority_neurology", "priority_pediatrics",
-            "priority_general_practitioner"
-        ]
+                  "priority_physiotherapy", "priority_cardiology",
+                  "priority_neurology", "priority_pediatrics",
+                  "priority_general_practitioner"]
