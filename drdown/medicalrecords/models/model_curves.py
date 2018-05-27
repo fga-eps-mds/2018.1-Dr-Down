849b54df92843f1ad56293a9bb76109e5659fec9
GeovanaRamos <geovannna.1998@gmail.com>
2018-05-27 10:27:04 -0300

    Adding Height History
    Adding class height history
    
    Adding method to return patient height history
    
    Co-authored-by: Guilherme Guy <guilhermeguy349@gmail.com>

--- drdown/medicalrecords/models/model_curves.py
+++ drdown/medicalrecords/models/model_curves.py
@@ -10,35 +10,41 @@ class Curves(models.Model):
     patient = models.ForeignKey(
         Patient,
         on_delete=models.CASCADE,
-        null=True,
-        blank=True,
         verbose_name=_('Patient')
     )
 
     __original_weight = None
+    __original_height = None
 
     def __init__(self, *args, **kwargs):
         super(Curves, self).__init__(*args, **kwargs)
         self.__original_weight = self.weight
+        self.__original_height = self.height
 
     def save(self, force_insert=False, force_update=False, *args, **kwargs):
         if self.weight != self.__original_weight:
             # weight changed
             new_weight = WeightHistory(patient=self.patient, weight=self.weight)
             new_weight.save()
+        elif self.height != self.__original_height:
+            # height changed
+            new_height = HeightHistory(patient=self.patient, height=self.weight)
+            new_height.save()
         super(Curves, self).save(force_insert, force_update, *args, **kwargs)
         self.__original_weight = self.weight
+        self.__original_height = self.height
 
     def weight_history(self):
         return WeightHistory.objects.filter(patient=self.patient).order_by('-age')
+
+    def height_history(self):
+        return HeightHistory.objects.filter(patient=self.patient).order_by('-age')
 
 
 class WeightHistory(models.Model):
     patient = models.ForeignKey(
         Patient,
         on_delete=models.CASCADE,
-        null=True,
-        blank=True,
         verbose_name=_('Patient')
     )
     weight = models.IntegerField()
@@ -55,3 +61,25 @@ class WeightHistory(models.Model):
         # convert to months
         self.age = age_years * 12
         super(WeightHistory, self).save(*args, **kwargs)
+
+
+class HeightHistory(models.Model):
+    patient = models.ForeignKey(
+        Patient,
+        on_delete=models.CASCADE,
+        verbose_name=_('Patient')
+    )
+    height = models.IntegerField()
+    age = models.IntegerField()
+
+    class Meta:
+        unique_together = ('patient', 'age',)
+
+    def save(self, *args, **kwargs):
+        # get age in years
+        born = self.patient.user.birthday
+        today = timezone.today()
+        age_years = today.year - born.year - ((today.month, today.day) < (born.month, born.day))
+        # convert to months
+        self.age = age_years * 12
+        super(HeightHistory, self).save(*args, **kwargs)
