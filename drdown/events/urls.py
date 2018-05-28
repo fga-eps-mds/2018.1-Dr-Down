4c05440487408c9173323eac3838693048be4cf7
Gabriela Medeiros <gabriela.medeiros2010@hotmail.com.br>
2018-05-28 11:51:17 -0300

    Adding create functionality
    Adding CreateView in view
    
    Creating url
    
    Co-authored-by: joberth rogers <joberth.rogers18@gmail.com>

--- drdown/events/urls.py
+++ drdown/events/urls.py
@@ -1,12 +1,18 @@
 from django.conf.urls import url
 from drdown.events.views.view_event import EventsListView
+from drdown.events.views.view_event import EventsCreateView
 
 app_name ='events'
 
 urlpatterns = [
     url(
-        regex=r'^$',
-        view=EventsListView.as_view(),
-        name='list_events'
+        regex = r'^$',
+        view = EventsListView.as_view(),
+        name = 'list_events'
     ),
+    url(
+    	regex = r'^new/$',
+    	view = EventsCreateView.as_view(),
+    	name = 'create_event'
+    	),
 ]
--- drdown/events/views/view_event.py
+++ drdown/events/views/view_event.py
@@ -4,9 +4,28 @@ from django.views.generic import UpdateView
 from django.views.generic import DeleteView
 from django.views.generic import ListView
 from drdown.events.models.model_events import Events
+from django.urls import reverse
 
 class EventsListView(ListView):
     model = Events
     slug_field = 'name'
     slug_url_kwarg = 'name'
     template_name = 'events_list.html'
+
+class EventsCreateView(CreateView):
+	model = Events
+	template_name = 'events_form.html'
+	fields = [
+		'date',
+		'time',
+		'description',
+		'organize_by',
+		'value',
+	]
+	
+	def get_success_url(self):
+		print("Entrou no sucess url")
+		success_create_url = reverse(
+			viewname='events:list_events',
+        )
+		return success_create_url
