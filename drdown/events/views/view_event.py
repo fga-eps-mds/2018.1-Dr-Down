2d817ca158926c25accb22a709fab201d3c5f7ea
Joberth Rogers <joberth.rogers18@gmail.com>
2018-05-28 09:59:15 -0300

    Startin' view from event
    list view especificaly
    
    Co-authored-by: Gabriela Medeiros <gabriela.medeiros2010@hotmail.com.br>

--- drdown/events/views/view_event.py
+++ drdown/events/views/view_event.py
@@ -2,9 +2,11 @@ from django.utils.translation import ugettext_lazy as _
 from django.views.generic import CreateView
 from django.views.generic import UpdateView
 from django.views.generic import DeleteView
-from .models.model_events import Events
+from django.views.generic import ListView
+from drdown.events.models.model_events import Events
 
-class EventList(ListView):
+class EventsListView(ListView):
     model = Events
     slug_field = 'name'
     slug_url_kwarg = 'name'
+    template_name = 'events_list.html'
