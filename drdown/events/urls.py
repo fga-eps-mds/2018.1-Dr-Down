45784c9a66a5d80462969ae1a5cc96a705362102
Joberth Rogers <joberth.rogers18@gmail.com>
2018-05-28 09:56:03 -0300

    Basic configs
    Url for list view
    
    Adding a potuatuin in base
    
    Adding __init__ in view
    
    Co-authored-by: Gabriela Medeiros <gabriela.medeiros2010@hotmail.com.br>

--- drdown/events/urls.py
+++ drdown/events/urls.py
@@ -1,5 +1,12 @@
 from django.conf.urls import url
+from drdown.events.views.view_event import EventsListView
 
 app_name ='events'
 
-urlpatterns = []
+urlpatterns = [
+    url(
+        regex=r'^$',
+        view=EventsListView.as_view(),
+        name='list_events'
+    ),
+]
