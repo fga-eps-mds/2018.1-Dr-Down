fc72bebdf0020f3b432a99046a4a10b15fab5852
Joberth Rogers <joberth.rogers18@gmail.com>
2018-05-28 14:56:43 -0300

    Update view
    url from event update
    
    view ate the same place
    
    Co-authored-by: Gabriela Medeiros <gabriela.medeiros2010@hotmail.com.br>

--- drdown/events/urls.py
+++ drdown/events/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from drdown.events.views.view_event import EventsListView
 from drdown.events.views.view_event import EventsCreateView
+from drdown.events.views.view_event import EventsUpdateView
 
 app_name ='events'
 
@@ -14,5 +15,10 @@ urlpatterns = [
     	regex = r'^new/$',
     	view = EventsCreateView.as_view(),
     	name = 'create_event'
-    	),
+     ),
+    url(
+    	regex = r'^update/(?P<pk>\d+)/$',
+    	view = EventsUpdateView.as_view(),
+    	name = 'update_event'
+     ),
 ]
--- drdown/events/views/view_event.py
+++ drdown/events/views/view_event.py
@@ -5,6 +5,7 @@ from django.views.generic import DeleteView
 from django.views.generic import ListView
 from drdown.events.models.model_events import Events
 from django.urls import reverse
+from django.urls import reverse_lazy
 
 class EventsListView(ListView):
     model = Events
@@ -13,19 +14,43 @@ class EventsListView(ListView):
     template_name = 'events_list.html'
 
 class EventsCreateView(CreateView):
-	model = Events
-	template_name = 'events_form.html'
-	fields = [
+    model = Events
+    template_name = 'events_form.html'
+    fields = [
 		'date',
 		'time',
 		'description',
 		'organize_by',
 		'value',
 	]
-	
-	def get_success_url(self):
-		print("Entrou no sucess url")
-		success_create_url = reverse(
+
+    def get_success_url(self):
+
+      success_create_url = reverse(
 			viewname='events:list_events',
+      )
+      return success_create_url
+
+class EventsUpdateView(UpdateView):
+    model = Events
+    template_name = 'events_form.html'
+    fields = [
+        'date',
+        'time',
+        'description',
+        'organize_by',
+        'value',
+    ]
+
+    def get_object(self):
+        event = Events.objects.get(
+            pk=self.kwargs.get('pk')
         )
-		return success_create_url
+        return event
+
+    success_url = reverse_lazy(
+        viewname='events:list_events',
+    )
+
+
+
