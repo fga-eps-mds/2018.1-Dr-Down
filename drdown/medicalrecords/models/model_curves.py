0b993713d53f907c56905ae8487f71c9a7cc32bd
guilherme1guy <guilhermeguy349@gmail.com>
2018-05-28 13:22:01 -0300

    Updating models
    Separating weight, height  and bmi models
    
    Co-authored-by: GeovanaRamos <geovannna.1998@gmail.com>

--- drdown/medicalrecords/models/model_curves.py
+++ drdown/medicalrecords/models/model_curves.py
@@ -2,113 +2,64 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from django.utils import timezone
 from ...users.models.model_patient import Patient
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.apps import apps
 
 
 class Curves(models.Model):
-    weight = models.IntegerField()
-    height = models.IntegerField()
-    patient = models.ForeignKey(
+
+    patient = models.OneToOneField(
         Patient,
         on_delete=models.CASCADE,
         verbose_name=_('Patient')
     )
 
-    __original_weight = None
-    __original_height = None
+class Weight(models.Model):
 
-    def __init__(self, *args, **kwargs):
-        super(Curves, self).__init__(*args, **kwargs)
-        self.__original_weight = self.weight
-        self.__original_height = self.height
-
-    def save(self, force_insert=False, force_update=False, *args, **kwargs):
-        if self.weight != self.__original_weight:
-            # weight changed
-            new_weight = WeightHistory(
-                patient=self.patient,
-                age=self.get_age,
-                weight=self.weight
-            )
-            new_weight.save()
-            new_bmi = BMIHistory(
-                patient=self.patient,
-                age=self.get_age,
-                bmi=self.get_bmi
-            )
-            new_bmi.save()
-        elif self.height != self.__original_height:
-            # height changed
-            new_height = HeightHistory(
-                patient=self.patient,
-                age=self.get_age,
-                height=self.weight
-            )
-            new_height.save()
-            new_bmi = BMIHistory(
-                patient=self.patient,
-                age=self.get_age,
-                bmi=self.get_bmi
-            )
-            new_bmi.save()
-        super(Curves, self).save(force_insert, force_update, *args, **kwargs)
-        self.__original_weight = self.weight
-        self.__original_height = self.height
-
-    def get_bmi(self):
-        return self.weight / (self.height * self.height)
-
-    def get_age(self):
-        # get age in years
-        born = self.patient.user.birthday
-        today = timezone.today()
-        age_years = today.year - born.year - ((today.month, today.day) < (born.month, born.day))
-        # convert to months
-        self.age = age_years * 12
-
-    def weight_history(self):
-        return WeightHistory.objects.filter(patient=self.patient).order_by('-age')
-
-    def height_history(self):
-        return HeightHistory.objects.filter(patient=self.patient).order_by('-age')
-
-    def bmi_history(self):
-        return BMIHistory.objects.filter(patient=self.patient).order_by('-age')
-
-
-class WeightHistory(models.Model):
-    patient = models.ForeignKey(
-        Patient,
+    curves = models.ForeignKey(
+        Curves,
         on_delete=models.CASCADE,
         verbose_name=_('Patient')
     )
-    weight = models.IntegerField()
+
+    weight = models.FloatField()
     age = models.IntegerField()
 
     class Meta:
-        unique_together = ('patient', 'age',)
+        unique_together = ('curves', 'age',)
 
 
-class HeightHistory(models.Model):
-    patient = models.ForeignKey(
-        Patient,
+class Height(models.Model):
+
+    curves = models.ForeignKey(
+        Curves,
         on_delete=models.CASCADE,
         verbose_name=_('Patient')
     )
+
     height = models.IntegerField()
     age = models.IntegerField()
 
     class Meta:
-        unique_together = ('patient', 'age',)
+        unique_together = ('curves', 'age',)
 
 
-class BMIHistory(models.Model):
-    patient = models.ForeignKey(
-        Patient,
+class BMI(models.Model):
+
+    curves = models.ForeignKey(
+        Curves,
         on_delete=models.CASCADE,
         verbose_name=_('Patient')
     )
-    bmi = models.IntegerField()
+
+    bmi = models.FloatField()
     age = models.IntegerField()
 
+    @staticmethod
+    def calculate_bmi(weight, height):
+        return weight / ((height/100)**2)
+
     class Meta:
-        unique_together = ('patient', 'age',)
+        unique_together = ('curves', 'age',)
+        verbose_name = _("BMI")
