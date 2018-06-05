595267dfa2376515dfb932f6e273780d751700f2
guilherme1guy <guilhermeguy349@gmail.com>
2018-06-05 12:43:05 -0300

    Changes in cuves js
    Optimizing function calls
    
    Changing graphic display
    
    Co-authored-by: Elias Bernardo <ebmm01@gmail.com>

--- drdown/medicalrecords/static/medicalrecords/js/curves.js
+++ drdown/medicalrecords/static/medicalrecords/js/curves.js
@@ -35,11 +35,14 @@ $("#height_chart").ready(
             url:"curves/ajax/",
             data: {
                 'username': document.getElementById("_username").value,
-                'data_type': 'height'
+                'data_type': 'height',
+                'time_frame' : 'months'
             },
             success: function(response){
                 height_data = JSON.stringify(response.data);
-                ready();
+                
+                google.charts.load('current', {'packages':['corechart']});
+                google.charts.setOnLoadCallback(draw_HeigthChart);
             },
             error: function(response){
                 console.log(response);
@@ -61,7 +64,9 @@ $("#weight_chart").ready(
             },
             success: function(response){
                 weight_data = JSON.stringify(response.data);
-                ready();
+                
+                google.charts.load('current', {'packages':['corechart']});
+                google.charts.setOnLoadCallback(draw_WeigthChart);
             },
             error: function(response){
                 console.log(response);
@@ -83,7 +88,9 @@ $("#bmi_chart").ready(
             },
             success: function(response){
                 bmi_data = JSON.stringify(response.data);
-                ready();
+                
+                google.charts.load('current', {'packages':['corechart']});
+                google.charts.setOnLoadCallback(draw_BMIChart);
             },
             error: function(response){
                 console.log(response);
@@ -105,7 +112,9 @@ $("#perimeter_chart").ready(
             },
             success: function(response){
                 perimeter_data = JSON.stringify(response.data);
-                ready();
+
+                google.charts.load('current', {'packages':['corechart']});
+                google.charts.setOnLoadCallback(draw_PerimeterChart);
             },
             error: function(response){
                 console.log(response);
@@ -114,13 +123,6 @@ $("#perimeter_chart").ready(
 
     }
 )
-
-function ready(){
-
-    google.charts.load('current', {'packages':['corechart']});
-    google.charts.setOnLoadCallback(drawChart);
-
-}
 
 function convertToArray(string) {
 
@@ -151,19 +153,65 @@ function convertToArray(string) {
     return array
 }
 
-function defineOptions(title) {
+var DATA_TYPES = {
+    height: 'Height',
+    weight: 'Weight',
+    bmi: 'BMI',
+    perimeter: 'Perimeter'
+}
+
+function defineOptions(data_type) {
+    
+    title = data_type;
+    hAxis_title = 'Ages';
+    vAxis_title = data_type;
+    
+    var h_values = [0,0];
+    var v_values = [0,0];
+
+    switch (data_type) {
+        case DATA_TYPES.height:
+            
+            v_values[1] = 200;
+            
+            h_values[1] = 18*12;
+            break;
+        case DATA_TYPES.weight:
+            v_values[1] = 250;
+
+
+            h_values[1] = 18*12;
+            break;
+        case DATA_TYPES.bmi:
+            v_values[0] = 10;
+            v_values[1] = 50;
+
+            h_values[0] = 3;
+            h_values[1] = 18;
+            break;
+        case DATA_TYPES.perimeter:
+            v_values[0] = 10;
+            v_values[1] = 50;
+
+            h_values[1] = 24;
+            break;
+    
+        default:
+            break;
+    }
+
     return {
         title: title,
+        curveType: 'function',
         hAxis: {
-            title: 'Age',
+            title: hAxis_title,
             titleTextStyle: {color: '#333'},
-            minValue: 25
         },
         vAxis: {
-            title: 'Height',
+            title: vAxis_title,
             titleTextStyle: {color: '#333'},
-            minValue: 0,
             format: 'decimal',
+            viewWindowMode: 'pretty',
         },
         explorer: {
             actions: ['dragToZoom', 'rightClickToReset'],
@@ -182,34 +230,44 @@ function defineOptions(title) {
             5: { color: '#e7711b' },
             6: { color: '#e2431e' },
         },
-        is3D: true
+        is3D: false
     };
 }
 
-function drawChart() {
+function draw_HeigthChart() {
 
     var height_array = convertToArray(height_data)
     var data_height = google.visualization.arrayToDataTable(height_array);
-    var options = defineOptions(title='Height')
-    var height_chart = new google.visualization.AreaChart(document.getElementById('height_chart'));
+    var options = defineOptions(data_type=DATA_TYPES.height)
+    var height_chart = new google.visualization.LineChart(document.getElementById('height_chart'));
     height_chart.draw(data_height, options);
+
+}
+
+function draw_WeigthChart() {
 
     var weight_array = convertToArray(weight_data)
     var data_weight = google.visualization.arrayToDataTable(weight_array);
-    var options = defineOptions(title='Weight')
-    var weight_chart = new google.visualization.AreaChart(document.getElementById('weight_chart'));
+    var options = defineOptions(data_type=DATA_TYPES.weight)
+    var weight_chart = new google.visualization.LineChart(document.getElementById('weight_chart'));
     weight_chart.draw(data_weight, options);
+}
+
+function draw_BMIChart() {
 
     var bmi_array = convertToArray(bmi_data)
     var data_bmi = google.visualization.arrayToDataTable(bmi_array);
-    var options = defineOptions(title='BMI')
-    var bmi_chart = new google.visualization.AreaChart(document.getElementById('bmi_chart'));
+    var options = defineOptions(data_type=DATA_TYPES.bmi)
+    var bmi_chart = new google.visualization.LineChart(document.getElementById('bmi_chart'));
     bmi_chart.draw(data_bmi, options);
+}
+
+function draw_PerimeterChart() {
 
     var perimeter_array = convertToArray(perimeter_data)
     var data_perimeter = google.visualization.arrayToDataTable(perimeter_array);
-    var options = defineOptions(title='Perimeter')
-    var perimeter_chart = new google.visualization.AreaChart(document.getElementById('perimeter_chart'));
+    var options = defineOptions(data_type=DATA_TYPES.perimeter)
+    var perimeter_chart = new google.visualization.LineChart(document.getElementById('perimeter_chart'));
     perimeter_chart.draw(data_perimeter, options);
 
 }
