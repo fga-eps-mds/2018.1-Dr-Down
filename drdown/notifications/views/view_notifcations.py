b543c56ba56834e6f3d505c22e2a87db55b1aecc
GeovanaRamos <geovannna.1998@gmail.com>
2018-06-06 13:16:31 -0300

    Separating user views
    Adding view to other users notifications
    
    Adding alert to patient html
    
    Co-authored-by: joberth costa <joberth.rogers18@gmail.com>

--- drdown/notifications/views/view_notifcations.py
+++ drdown/notifications/views/view_notifcations.py
@@ -1,7 +1,8 @@
 from django.views.generic import TemplateView
+from drdown.appointments.models.model_request import AppointmentRequest
 from drdown.appointments.models.model_appointment import Appointment
 from drdown.events.models.model_events import Events
-from datetime import  timedelta
+from datetime import timedelta
 from django.utils import timezone
 
 
@@ -13,14 +14,76 @@ class PatientNotificationsView(TemplateView):
         context = super(PatientNotificationsView, self).get_context_data(**kwargs)
 
         user = self.request.user
-        context['appointments'] = Appointment.objects.filter(
+        context['scheduled'] = AppointmentRequest.objects.filter(
+            status=Appointment.SCHEDULED,
+        ).filter(
             patient=user.patient,
         )
 
         start_date = timezone.now()
         end_date = start_date + timedelta(days=6)
         context['events'] = Events.objects.filter(
-            date__range= (start_date, end_date)
+            date__range=(start_date, end_date)
+        )
+
+        return context
+
+
+class ResponsibleNotificationsView(TemplateView):
+    template_name = 'notifications/responsible_notifications.html'
+
+    def get_context_data(self, **kwargs):
+
+        context = super(ResponsibleNotificationsView, self).get_context_data(**kwargs)
+
+        user = self.request.user
+        context['appointments'] = Appointment.objects.filter(
+            patient__in=user.responsible.patient_set.all(),
+        )
+
+        start_date = timezone.now()
+        end_date = start_date + timedelta(days=6)
+        context['events'] = Events.objects.filter(
+            date__range=(start_date, end_date)
+        )
+
+        return context
+
+
+class HealthTeamNotificationsView(TemplateView):
+    template_name = 'notifications/health_team_notifications.html'
+
+    def get_context_data(self, **kwargs):
+
+        context = super(HealthTeamNotificationsView, self).get_context_data(**kwargs)
+
+        user = self.request.user
+        context['appointments'] = Appointment.objects.filter(
+            doctor=user.doctor,
+        )
+
+        start_date = timezone.now()
+        end_date = start_date + timedelta(days=6)
+        context['events'] = Events.objects.filter(
+            date__range=(start_date, end_date)
+        )
+
+        return context
+
+
+class EmployeeNotificationsView(TemplateView):
+    template_name = 'notifications/employee_notifications.html'
+
+    def get_context_data(self, **kwargs):
+
+        context = super(EmployeeNotificationsView, self).get_context_data(**kwargs)
+
+        context['requests'] = AppointmentRequest.objects.all()
+
+        start_date = timezone.now()
+        end_date = start_date + timedelta(days=6)
+        context['events'] = Events.objects.filter(
+            date__range=(start_date, end_date)
         )
 
         return context
