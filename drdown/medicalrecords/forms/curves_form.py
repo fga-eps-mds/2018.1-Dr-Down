e3ddc8166c1e13dae3fd20bd68fe2b9c80a088bd
Elias Bernardo <ebmm01@gmail.com>
2018-06-06 11:23:38 -0300

    Adding min_value to all Curves fields
    A curve field can not be less than 0 now
    
    Co-authored-by: Guilherme Guy <guilhermeguy349@gmail.com>

--- drdown/medicalrecords/forms/curves_form.py
+++ drdown/medicalrecords/forms/curves_form.py
@@ -6,6 +6,14 @@ from drdown.medicalrecords.models.model_curves import (
 
 class CurvesForm(forms.ModelForm):
 
+    weight = forms.FloatField(min_value=0.0)
+
+    height = forms.IntegerField(min_value=0)
+
+    cephalic_perimeter = forms.FloatField(min_value=0.0)
+
+    age = forms.IntegerField(min_value=0)
+
     class Meta:
         model = Curves
         fields = ["height", "weight", "age", "cephalic_perimeter", ]
