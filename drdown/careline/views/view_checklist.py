4ce80a1e33dad7205b4ca0100e71cbd934ff6057
guilherme1guy <guilhermeguy349@gmail.com>
2018-06-19 12:52:20 -0300

    Fixing permission error on careline
    Now a Employee can't change the carline data (only view it)
    
    Co-authored-by: Joberth Rogers <joberth.rogers18@gmail.com>

--- drdown/careline/views/view_checklist.py
+++ drdown/careline/views/view_checklist.py
@@ -110,7 +110,11 @@ class ChecklistUpdateView(RedirectView):
                 target_user=target_user
             )
 
-        return (allowed_on_view and current_user.age() >= 13)
+        return (
+            allowed_on_view and
+            current_user.age() >= 13 and
+            hasattr(current_user, 'employee') is False
+        )
 
     def post(self, request, *args, **kwargs):
 
@@ -131,8 +135,13 @@ class ChecklistUpdateView(RedirectView):
                 value=request.POST.get('value')
             )
         else:
-            message = _("Error: You cannot change data on this form. "
-                        "You need to be at least 13 years old.")
+            message = str(_("Error: You cannot change data on this form."))
+
+            if hasattr(request.user, 'employee'):
+                message += str(_(" You don't have permission."))
+            else:
+                message += str(_(" You need to be at least 13 years old."))
+
             return HttpResponseForbidden(message)
 
         return HttpResponse(message)
